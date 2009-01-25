138aec12cbf161c809d86251e1fca93a118cee6d
Dan Creswell <dan.creswell@gmail.com>
2009-01-25 14:08:03 +0000

    Apply script precedence when running them as part of aggregation.

--- src/org/dancres/gossip/astrolabe/MibAggregator.java
+++ src/org/dancres/gossip/astrolabe/MibAggregator.java
@@ -12,62 +12,73 @@ import org.slf4j.LoggerFactory;
 /**
  * This class implements the aggregation logic that generates a <code>Mib</code> for a zone from scripts in the
  * child zones.  The full multi-level aggregation process is implemented by {@link AggregationProcess}
- * 
- * @todo Apply logic to figure out the latest version of script and run that rather than whichever version we saw
- * last during scanning for scripts.
  */
 public class MibAggregator {
-	private static Logger _logger = LoggerFactory.getLogger(MibAggregator.class);
-	
-	private Zone _zone;
-	
-	public MibAggregator(Zone aZone) {
-		_zone = aZone;
-	}
-	
-	public void run() {
-		/*
-		 * Get all child zones, scan the mibs for attributes beginning with SCRIPT_NAME_PREDICATE (indicating a script).
-		 * Take each discovered script and apply it to the Mibs of the child zones, storing the results in the
-		 * Mib of _zone.
-		 */
-		
-		HashMap<String, Script> myScriptMap = new HashMap<String, Script>();
-		Collection<Mib> myMibList = new ArrayList<Mib>();		
-		Collection<Zone> myZoneList = _zone.getChildren();
-		Iterator<Zone> myZones = myZoneList.iterator();
-		
-		while (myZones.hasNext()) {
-			Zone myZone = myZones.next();
-			
-			_logger.debug("Examining: " + _zone + " -> " + myZone);
-			
-			Mib myZoneMib = myZone.getMib();
-			myMibList.add(myZoneMib);
-			
-			Attributes myAttrMap = myZoneMib.getAttributes();
-			Iterator<String> myKeys = myAttrMap.getKeys();
-			
-			while (myKeys.hasNext()) {
-				String myKey = myKeys.next();
-				_logger.debug("Checking: " + myZone.getName() + " Mib key: " + myKey);
-				
-				if (myKey.startsWith(Script.NAME_PREDICATE)) {
-					myScriptMap.put(myKey, (Script) myAttrMap.get(myKey));
-					_logger.debug("Keeping: " + myKey);
-				}
-			}
-		}
-		
-		long myIssued = _zone.getMib().getIssued();		
-		Iterator<String> myScriptNames = myScriptMap.keySet().iterator();
-		
-		while (myScriptNames.hasNext()) {
-			String myScriptName = myScriptNames.next();			
-			Script myScript = myScriptMap.get(myScriptName);
-			
-			try {
-				_logger.debug("Evaluating: " + myScriptName);
+
+    private static Logger _logger = LoggerFactory.getLogger(MibAggregator.class);
+    private Zone _zone;
+
+    public MibAggregator(Zone aZone) {
+        _zone = aZone;
+    }
+
+    public void run() {
+        /*
+         * Get all child zones, scan the mibs for attributes beginning with SCRIPT_NAME_PREDICATE (indicating a script).
+         * Take each discovered script and apply it to the Mibs of the child zones, storing the results in the
+         * Mib of _zone.
+         */
+
+        HashMap<String, Script> myScriptMap = new HashMap<String, Script>();
+        Collection<Mib> myMibList = new ArrayList<Mib>();
+        Collection<Zone> myZoneList = _zone.getChildren();
+        Iterator<Zone> myZones = myZoneList.iterator();
+
+        while (myZones.hasNext()) {
+            Zone myZone = myZones.next();
+
+            _logger.debug("Examining: " + _zone + " -> " + myZone);
+
+            Mib myZoneMib = myZone.getMib();
+            myMibList.add(myZoneMib);
+
+            Attributes myAttrMap = myZoneMib.getAttributes();
+            Iterator<String> myKeys = myAttrMap.getKeys();
+
+            while (myKeys.hasNext()) {
+                String myKey = myKeys.next();
+                _logger.debug("Checking: " + myZone.getName() + " Mib key: " + myKey);
+
+                if (myKey.startsWith(Script.NAME_PREDICATE)) {
+                    Script myCurrent = myScriptMap.get(myKey);
+                    Script myNewScript = (Script) myAttrMap.get(myKey);
+
+                    // Apply precedence
+                    //
+                    if (myCurrent == null) {
+                        _logger.debug("Adding: " + myKey);
+                        myScriptMap.put(myKey, (Script) myAttrMap.get(myKey));
+                    } else {
+                        if (myNewScript.isPreferable(myCurrent)) {
+                            _logger.debug("Replacing: " + myKey);
+                            myScriptMap.put(myKey, (Script) myAttrMap.get(myKey));
+                        } else {
+                            _logger.debug("Keeping: " + myKey);
+                        }
+                    }
+                }
+            }
+        }
+
+        long myIssued = _zone.getMib().getIssued();
+        Iterator<String> myScriptNames = myScriptMap.keySet().iterator();
+
+        while (myScriptNames.hasNext()) {
+            String myScriptName = myScriptNames.next();
+            Script myScript = myScriptMap.get(myScriptName);
+
+            try {
+                _logger.debug("Evaluating: " + myScriptName);
 
                 /*
                  * We don't want scripts directly updating the Mib attributes of the zone.  This is because we have to control/limit
@@ -76,7 +87,7 @@ public class MibAggregator {
                  */
                 Mib myZoneMib = _zone.getMib();
                 Mib myTempMib = myZoneMib.dup();
-				myScript.evaluate(myMibList, myTempMib);
+                myScript.evaluate(myMibList, myTempMib);
                 Iterator<String> myAttrKeys = myTempMib.getAttributes().getKeys();
 
                 while (myAttrKeys.hasNext()) {
@@ -87,14 +98,15 @@ public class MibAggregator {
                         myZoneMib.getAttributes().put(myKey, myValue);
                     }
                 }
-			} catch (Exception anE) {
-				_logger.warn("Failed to evaluate script: " + myScriptName, anE);
-			}
-		}
-		
-		if (_zone.getMib().getIssued() == myIssued)
-			_zone.getMib().setIssued(System.currentTimeMillis());
-	}
+            } catch (Exception anE) {
+                _logger.warn("Failed to evaluate script: " + myScriptName, anE);
+            }
+        }
+
+        if (_zone.getMib().getIssued() == myIssued) {
+            _zone.getMib().setIssued(System.currentTimeMillis());
+        }
+    }
 
     private boolean canPropogate(Zone aZone, String aKey, Object aValue) {
         // Only propogate a script if it's origin zone matches this Zone or a parent of this Zone
@@ -105,8 +117,9 @@ public class MibAggregator {
             Zone myCurrent = aZone;
 
             do {
-                if (myOrigin.equals(myCurrent.getId()))
+                if (myOrigin.equals(myCurrent.getId())) {
                     return true;
+                }
                 myCurrent = myCurrent.getParent();
             } while (myCurrent != null);
 
