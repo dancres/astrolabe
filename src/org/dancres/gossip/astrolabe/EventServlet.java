01755565fa30356260e203445708be9a7db5a9f9
Dan Creswell <dan.creswell@gmail.com>
2009-02-15 18:23:16 +0000

    Add appropriate URL-space commentary.

--- src/org/dancres/gossip/astrolabe/EventServlet.java
+++ src/org/dancres/gossip/astrolabe/EventServlet.java
@@ -9,6 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * And update events (zone additions/removals or attribute updates are accessible as follows:
+ * <ol>
+ * <li>The event servlet endpoint</li>
+ * <li>An additional path element which is the zone of interest</li>
+ * </ol>
+ *
+ * The content of the response will be a list of names for child zones of the zone specified in the URL.
+ */
 public class EventServlet extends HttpServlet {
 	public static final String MOUNT_POINT = "events/*";
 	public static final String PATH = "events";
