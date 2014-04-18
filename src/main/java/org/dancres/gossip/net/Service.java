c26d2b5261156d3b341828ab9dbd0747739810b9
Dan Creswell <dan.creswell@gmail.com>
2014-04-18 11:51:32 +0100

    Update to non-deprecated http apache methods.

--- src/main/java/org/dancres/gossip/net/Service.java
+++ src/main/java/org/dancres/gossip/net/Service.java
@@ -4,22 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServlet;
 
-import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.dancres.gossip.discovery.HostDetails;
-import org.dancres.gossip.peersampling.GossipServlet;
-import org.dancres.gossip.peersampling.RemotePeerSampler;
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.bio.SocketConnector;
@@ -36,7 +24,6 @@ public class Service {
 
     private static Logger _logger = LoggerFactory.getLogger(Service.class);
     private HttpClient _client;
-    private SocketConnector _connector;
     private Server _server;
     private ServletHandler _handler;
     private int _port;
@@ -52,21 +39,10 @@ public class Service {
 
         _root = aRoot;
 
-        HttpParams params = new BasicHttpParams();
-        HttpConnectionParams.setConnectionTimeout(params, 20 * 1000);
-        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+        HttpClientBuilder myBuilder = HttpClientBuilder.create();
+        myBuilder.setConnectionManager(new PoolingHttpClientConnectionManager());
 
-        // Create and initialize scheme registry
-        SchemeRegistry schemeRegistry = new SchemeRegistry();
-        schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-        schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
-
-        // Create an HttpClient with the ThreadSafeClientConnManager.
-        // This connection manager must be used if more than one thread will
-        // be using the HttpClient.
-        ClientConnectionManager cm = new ThreadSafeClientConnManager(params, schemeRegistry);
-
-        _client = new DefaultHttpClient(cm, params);
+        _client = myBuilder.build();
 
         _server = new Server();
         SocketConnector myConnector = new SocketConnector();
